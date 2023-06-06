c563a85977fcf83b393569584df2bffd5fb9e3f9
sim8191 <sim8191si@naver.com>
2023-06-06 20:04:42 +0900

    initial commit

--- pybo/models.py
+++ pybo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
