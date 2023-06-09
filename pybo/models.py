a51bc70921080a44c430e1dbe29983b2949f1e0f
sim8191 <sim8191si@naver.com>
2023-06-10 02:19:33 +0900

    initial commit

--- pybo/models.py
+++ pybo/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
